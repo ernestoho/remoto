07f00da711a5f785e6dfb57a275bc4c22182dcf5
AgistStudio <junior.ho@gmail.com>
2024-07-17 05:35:38 +0000

    update mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,14 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
+  // disable: process.env.NODE_ENV === "development",
+  // register: true,
+  // scope: "/app",
+  // sw: "service-worker.js",
+  //...
 });
 
+// Your Next config is automatically typed!
 export default withPWA({
   // Your Next.js config
 });
