42e58f9c8ca401ecbf543c4013a759383fce6886
AgistStudio <junior.ho@gmail.com>
2024-07-17 05:00:23 +0000

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
-/** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
 
+const withPWA = withPWAInit({
+  dest: "public",
+});
 
-const nextConfig = {
-    output: "export",
-    trailingSlash:true,
-    images: {
-        loader: "custom",
-        loaderFile: './ImageLoader.js'
-    },
-};
-
-export default nextConfig;
+export default withPWA({
+  // Your Next.js config
+});
